d429066de12856f6c17275f774f2e730e66869b3
Lev Brouk <lev@synadia.com>
2023-06-18 05:54:29 -0700

    a11

--- .github/hack/merge-branch.sh
+++ .github/hack/merge-branch.sh
@@ -1,57 +1,27 @@
 #!/usr/bin/env bash
 
-# Copyright 2015 The Kubernetes Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# Usage Instructions: https://git.k8s.io/community/contributors/devel/sig-release/cherry-picks.md
-
-# Checkout a PR from GitHub. (Yes, this is sitting in a Git tree. How
-# meta.) Assumes you care about pulls from remote "upstream" and
-# checks them out to a branch named:
-#  automated-cherry-pick-of-<pr>-<target branch>-<timestamp>
-
-# Modified by Mattermost to do a real cherry-pick instead of applying patches
-# via git am
-#
-# Modified by Synadia.
-
 set -o errexit
 set -o nounset
 set -o pipefail
-set -x
+
+# <>/<>
+cat ${GITHUB_EVENT_PATH}
+
+
+declare -r BRANCH="$1"
+declare -r PR_NUMBER="$2"
+declare -r MERGE_COMMIT_SHA="$3"
 
 GITHUB_ORG="${GITHUB_ORG:-levb}"
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
-# <>/<>
-# pwd 
-export
-# echo '<>/<>--------------------'
-# cat ${GITHUB_EVENT_PATH}
-# echo '<>/<>--------------------'
-
-declare -r BRANCH="$1"
-declare -r PR_NUMBER="$2"
-declare -r COMMIT_SHA="$3"
-
 NEW_BRANCH="automerge-to-${BRANCH}-pr${PR_NUMBER}-$(date +%s)"
 declare -r NEW_BRANCH
-echo "+++ Create local branch ${NEW_BRANCH} for PR #${PR_NUMBER} at ${COMMIT_SHA}"
-git checkout -b "${NEW_BRANCH}" "${COMMIT_SHA}"
+echo "+++ Create local branch ${NEW_BRANCH} for PR #${PR_NUMBER} at ${MERGE_COMMIT_SHA}"
+git checkout -b "${NEW_BRANCH}" "${MERGE_COMMIT_SHA}"
 
-echo "+++ Try merging ${COMMIT_SHA} onto ${BRANCH}"
+echo "+++ Try merging ${MERGE_COMMIT_SHA} onto ${BRANCH}"
 git checkout "${BRANCH}"
 set +o errexit
 git merge --no-ff "${NEW_BRANCH}"
@@ -73,7 +43,7 @@ git push origin "${NEW_BRANCH}"
 # crash.
 prtext="$(mktemp -t prtext.XXXX)" 
 cat >"${prtext}" <<EOF
-Failed merge: #${PR_NUMBER}
+Failed to merge #${PR_NUMBER} to ${BRANCH}
 
 Automated merge to ${BRANCH} triggered by #${PR_NUMBER} failed. Please resolve the conflicts and push manually, see [C Release Instructions](https://github.com/nats-io/nats-internal/blob/master/release-instructions/C.md)
 
