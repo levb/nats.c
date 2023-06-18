212c2fad9859f8e99a5e084a293f9dcf773c1961
Lev Brouk <lev@synadia.com>
2023-06-18 13:57:50 -0700

    change 4

--- .github/scripts/merge-branch.sh
+++ .github/scripts/merge-branch.sh
@@ -25,7 +25,6 @@ set -o errexit
 if [[ -z $(git status --porcelain) ]]; then
   echo "+++ Merged cleanly, push to GitHub."
   git push origin "${BRANCH}"
-
   gh pr comment "${PR_NUMBER}" --body "Automated merge to ${BRANCH} was clean :tada:. Please check the build status."
   exit 0
 fi
@@ -35,10 +34,11 @@ git merge --abort
 git checkout "${NEW_BRANCH}"
 git push origin "${NEW_BRANCH}"
 
-gh pr create \
+fix_pr_url=$(gh pr create \
   --title "Failed automated merge of #${PR_NUMBER}." \
   --body "Failed automated merge to ${BRANCH} triggered by #${PR_NUMBER}. Please resolve the conflicts and push manually, see [C Release Instructions](https://github.com/nats-io/nats-internal/blob/master/release-instructions/C.md)" \
   --head "${NEW_BRANCH}" \
-  --base "${BRANCH}"
+  --base "${BRANCH}")
+fix_pr_number=$(basename "${fix_pr_url}")
 
-gh pr comment "${PR_NUMBER}" --body "Automated merge to ${BRANCH} was clean :tada:. Please check the build status."
+gh pr comment "${PR_NUMBER}" --body "Automated merge to ${BRANCH} failed. Fix PR ${fix_pr_url} filed."
