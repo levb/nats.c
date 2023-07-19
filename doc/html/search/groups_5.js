2bb34e393d5f4fed183c4256d7c2f740a53d2781
cut-release for levb <levb@users.noreply.github.com>
2023-07-19 12:24:06 +0000

    Updated docs for v3.7.0

--- doc/html/search/groups_5.js
+++ doc/html/search/groups_5.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['keyvalue_20store_0',['KeyValue store',['../group__kv_group.html',1,'']]],
-  ['keyvalue_20store_20entries_1',['KeyValue store entries',['../group__kv_entry.html',1,'']]],
-  ['keyvalue_20store_20management_2',['KeyValue store management',['../group__kv_group_mgt.html',1,'']]],
-  ['keyvalue_20store_20status_3',['KeyValue store status',['../group__kv_status.html',1,'']]],
-  ['keyvalue_20store_20watcher_4',['KeyValue store watcher',['../group__kv_watcher.html',1,'']]]
+  ['keyvalue_20store_1829',['KeyValue store',['../group__kv_group.html',1,'']]],
+  ['keyvalue_20store_20entries_1830',['KeyValue store entries',['../group__kv_entry.html',1,'']]],
+  ['keyvalue_20store_20management_1831',['KeyValue store management',['../group__kv_group_mgt.html',1,'']]],
+  ['keyvalue_20store_20status_1832',['KeyValue store status',['../group__kv_status.html',1,'']]],
+  ['keyvalue_20store_20watcher_1833',['KeyValue store watcher',['../group__kv_watcher.html',1,'']]]
 ];
