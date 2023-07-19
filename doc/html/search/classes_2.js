0b14c211d6f9f4ccf8fe410f45ae912934d24654
cut-release for levb <levb@users.noreply.github.com>
2023-07-19 12:56:33 +0000

    Updated docs for v3.7.0

--- doc/html/search/classes_2.js
+++ doc/html/search/classes_2.js
@@ -1,4 +1,9 @@
 var searchData=
 [
-  ['natsmsglist_0',['natsMsgList',['../structnats_msg_list.html',1,'']]]
+  ['micro_5fendpoint_5fconfig_5fs_958',['micro_endpoint_config_s',['../structmicro__endpoint__config__s.html',1,'']]],
+  ['micro_5fendpoint_5finfo_5fs_959',['micro_endpoint_info_s',['../structmicro__endpoint__info__s.html',1,'']]],
+  ['micro_5fendpoint_5fstats_5fs_960',['micro_endpoint_stats_s',['../structmicro__endpoint__stats__s.html',1,'']]],
+  ['micro_5fservice_5fconfig_5fs_961',['micro_service_config_s',['../structmicro__service__config__s.html',1,'']]],
+  ['micro_5fservice_5finfo_5fs_962',['micro_service_info_s',['../structmicro__service__info__s.html',1,'']]],
+  ['micro_5fservice_5fstats_5fs_963',['micro_service_stats_s',['../structmicro__service__stats__s.html',1,'']]]
 ];
--- doc/html/search/groups_7.js
+++ doc/html/search/groups_7.js
@@ -1,6 +1,12 @@
 var searchData=
 [
-  ['management_0',['Management',['../group__conn_mgt_group.html',1,'(Global Namespace)'],['../group__stan_conn_mgt_group.html',1,'(Global Namespace)']]],
-  ['message_1',['Message',['../group__msg_group.html',1,'']]],
-  ['messages_2',['Messages',['../group__js_msg.html',1,'']]]
+  ['management_1837',['Management',['../group__conn_mgt_group.html',1,'(Global Namespace)'],['../group__stan_conn_mgt_group.html',1,'(Global Namespace)']]],
+  ['message_1838',['Message',['../group__msg_group.html',1,'']]],
+  ['messages_1839',['Messages',['../group__js_msg.html',1,'']]],
+  ['microclient_1840',['microClient',['../group__micro_client_functions.html',1,'']]],
+  ['microerror_1841',['microError',['../group__micro_error_functions.html',1,'']]],
+  ['microgroup_1842',['microGroup',['../group__micro_group_functions.html',1,'']]],
+  ['microrequest_1843',['microRequest',['../group__micro_request_functions.html',1,'']]],
+  ['microservice_1844',['microService',['../group__micro_service_functions.html',1,'']]],
+  ['miscellaneous_1845',['Miscellaneous',['../group__micro_cleanup_functions.html',1,'']]]
 ];
