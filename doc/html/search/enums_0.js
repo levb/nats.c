75cea79ed759b69a11840dd663c1fd45c45ec5fb
cut-release for levb <levb@users.noreply.github.com>
2023-07-19 12:52:02 +0000

    Updated docs for v3.7.0

--- doc/html/search/enums_0.js
+++ doc/html/search/enums_0.js
@@ -1,10 +1,10 @@
 var searchData=
 [
-  ['jsackpolicy_0',['jsAckPolicy',['../group__types_group.html#ga8fcb50e6e7312834791f9e237d28e873',1,'nats.h']]],
-  ['jsdeliverpolicy_1',['jsDeliverPolicy',['../group__types_group.html#ga117ed2c35f89ef3144540ed9cbadc818',1,'nats.h']]],
-  ['jsdiscardpolicy_2',['jsDiscardPolicy',['../group__types_group.html#ga7f88b7d482488398074f9d1419a9b8c2',1,'nats.h']]],
-  ['jserrcode_3',['jsErrCode',['../status_8h.html#a10731415ffcd3e2330df10c4d5fd3d70',1,'status.h']]],
-  ['jsreplaypolicy_4',['jsReplayPolicy',['../group__types_group.html#ga8650faf52b0b6b23df376eebeb9d0354',1,'nats.h']]],
-  ['jsretentionpolicy_5',['jsRetentionPolicy',['../group__types_group.html#ga410f8fb053efa02b2ca1de0c02fa4b6d',1,'nats.h']]],
-  ['jsstoragetype_6',['jsStorageType',['../group__types_group.html#ga1802a53c79e667f093bba033b16adc98',1,'nats.h']]]
+  ['jsackpolicy_1603',['jsAckPolicy',['../group__types_group.html#ga8fcb50e6e7312834791f9e237d28e873',1,'nats.h']]],
+  ['jsdeliverpolicy_1604',['jsDeliverPolicy',['../group__types_group.html#ga117ed2c35f89ef3144540ed9cbadc818',1,'nats.h']]],
+  ['jsdiscardpolicy_1605',['jsDiscardPolicy',['../group__types_group.html#ga7f88b7d482488398074f9d1419a9b8c2',1,'nats.h']]],
+  ['jserrcode_1606',['jsErrCode',['../status_8h.html#a10731415ffcd3e2330df10c4d5fd3d70',1,'status.h']]],
+  ['jsreplaypolicy_1607',['jsReplayPolicy',['../group__types_group.html#ga8650faf52b0b6b23df376eebeb9d0354',1,'nats.h']]],
+  ['jsretentionpolicy_1608',['jsRetentionPolicy',['../group__types_group.html#ga410f8fb053efa02b2ca1de0c02fa4b6d',1,'nats.h']]],
+  ['jsstoragetype_1609',['jsStorageType',['../group__types_group.html#ga1802a53c79e667f093bba033b16adc98',1,'nats.h']]]
 ];
