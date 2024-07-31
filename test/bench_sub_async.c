4768f63ee4253d08a0e2038de003a13b00061669
Lev Brouk <lev@synadia.com>
2024-07-31 08:12:07 -0700

    test adjustments, +11

--- test/bench_sub_async.c
+++ test/bench_sub_async.c
@@ -95,14 +95,15 @@ void test_BenchSubscribeAsync_Small(void)
 void test_BenchSubscribeAsync_Large(void)
 {
     threadConfig threads[] = {
-        {false, 1}, // 1 is not used in this case, just to quiet nats_SetMessageDeliveryPoolSize
-        {true, 5},
-        {true, 11},
+        // {false, 1}, // 1 is not used in this case, just to quiet nats_SetMessageDeliveryPoolSize
+        // {true, 5},
+        // {true, 11},
         {true, 23},
-        {true, 47}, // to compare to non-pooled
+        // {true, 47}, // to compare to non-pooled
     };
 
-    int subs[] = {1, 3, 4, 5, 7, 10,  17, 47};
+    int subs[] = { 17};
+    // int subs[] = {1, 3, 4, 5, 7, 10, 17, 47};
 
     ENV env = {
         .pubf = _publish,
