b13d38ebe6e694bd13458f4f38bacdb9026d01df
Lev Brouk <lev@synadia.com>
2024-07-31 08:13:57 -0700

    test adjustments, +12

--- test/bench_sub_async.c
+++ test/bench_sub_async.c
@@ -408,9 +408,9 @@ static natsStatus _publish(natsConnection *nc, const char *subject, ENV *env)
         snprintf(buf, sizeof(buf), "%d", i);
         IFOK(s, natsConnection_PublishString(nc, subject, buf));
 
-        if (i % 5) // progressive flush
+        if ((i != 0) && (i % flushAfter) == 0)
         {
-            printf("<>/<> flushing progressive: %d\n", i);
+            printf("<>/<> flushing: %d\n", i);
             IFOK(s, natsConnection_Flush(nc));
         }
         // if (i == (numPubMessages - 1)) // last message in batch
