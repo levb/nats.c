911db7c78e75409707a548b1ad450e210bfba81e
Lev Brouk <lev@synadia.com>
2024-08-08 10:39:36 -0700

    debugging pullsubasync on windows, +8

--- test/test.c
+++ test/test.c
@@ -28905,7 +28905,7 @@ void test_JetStreamSubscribePullAsync(void)
         {
             .name = "Fetch with expiration is fulfilled NATS_MAX_DELIVERED_MSGS",
             .want = 30,
-            .expires = 10, // ms
+            .expires = 100, // ms
             .before = 20,
             .during = 10,
             .expectedStatus = NATS_MAX_DELIVERED_MSGS,
